4d98a52cb5d8d70d575bcde9327b72afd36fe89d
Ye Lwin Oo <yelwinoo@ucsm.edu.mm>
2024-01-08 13:08:39 +0630

    added: greeting message and fixed late initialization issue

--- bin/discord_bot.dart
+++ bin/discord_bot.dart
@@ -1,6 +1,9 @@
 import 'package:discord_bot/jarvis.dart';
 
-void main() => Jarvis()
-  ..onInit()
-  ..run()
-  ..dispose();
+void main(){
+  final bot = Jarvis();
+  bot.onInit().then((_){
+    bot.run();
+    bot.dispose();
+  });
+}
--- lib/jarvis.dart
+++ lib/jarvis.dart
@@ -1,26 +1,29 @@
 import 'package:discord_bot/interface/jarvis_interface.dart';
-import 'package:nyxx/nyxx.dart';
+import 'package:nyxx/nyxx.dart' hide logging;
+import 'package:nyxx/src/plugin/logging.dart' show Logging;
 
 import 'configs/bot_key.dart';
 
 class Jarvis implements JarvisInterface {
+  late NyxxGateway client;
   @override
   void dispose() {
-    // TODO: implement dispose
+    // client.close();
   }
 
   @override
-  void onInit() {
-    // TODO: implement onInit
+  Future<void> onInit() async {
+    try {
+      client = await Nyxx.connectGateway(TOKEN, GatewayIntents.allUnprivileged,
+          options: GatewayClientOptions(plugins: [Logging(), cliIntegration]));
+    } catch (ex, stacktrace) {
+      Logging().logger.severe('Exception occurred $ex');
+      Logging().logger.severe(stacktrace.toString());
+    }
   }
 
   @override
   Future<void> run() async {
-    final client = await Nyxx.connectGateway(
-      TOKEN,
-      GatewayIntents.allUnprivileged,
-      options: GatewayClientOptions(plugins: [logging, cliIntegration]),
-    );
     final user = await client.users.fetchCurrentUser();
 
     client.onReady.listen((event) {
@@ -35,7 +38,13 @@ class Jarvis implements JarvisInterface {
 
     print("user is ${user.username}");
     client.onMessageCreate.listen((event) async {
-      if (event.mentions.contains(user)) {
+      print("----> mesage content ${event.message.author.username}");
+      if (event.message.author.username == 'yelwinoo') {
+        await event.message.channel.sendMessage(MessageBuilder(
+          content: 'Hello there! How are you? How may I assist you, Sir?',
+          replyId: event.message.id,
+        ));
+      } else if (event.mentions.contains(user)) {
         await event.message.channel.sendMessage(MessageBuilder(
           content: 'I love you!',
           replyId: event.message.id,
@@ -43,6 +52,4 @@ class Jarvis implements JarvisInterface {
       }
     });
   }
-
-
 }
